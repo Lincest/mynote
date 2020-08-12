974a321b9226c9ffcafe7ba93d5470a56c1e56e6
Lincest <474617032@qq.com>
2020-08-12 16:09:05 +0800

    autoupdate

--- push.bat
+++ push.bat
@@ -1,6 +1,6 @@
 @echo off
 title auto commit
-"C:\Program Files\Git\bin\git.exe" book sm
+call book sm
 "C:\Program Files\Git\bin\git.exe" add . 
 "C:\Program Files\Git\bin\git.exe" commit -m "autoupdate" 
 "C:\Program Files\Git\bin\git.exe" push -f origin master 
