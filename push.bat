97aec97f90d4630d9b7a97564fd793020e4a5d3c
Lincest <Lincest@users.noreply.github.com>
2020-10-13 10:39:48 +0800

    Updated by gitbook-action 2020-10-13 10:37:42

--- push.bat
+++ push.bat
@@ -1,8 +1,9 @@
 @echo off
+set /p value=请输入commit内容:
 title auto commit
 call book sm
 "C:\Program Files\Git\bin\git.exe" add . 
-"C:\Program Files\Git\bin\git.exe" commit -m "autoupdate" 
+"C:\Program Files\Git\bin\git.exe" commit -m "%value%" 
 "C:\Program Files\Git\bin\git.exe" push -f origin master 
 @echo github sync finished
 exit
